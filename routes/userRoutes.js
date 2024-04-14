1bb85bb0e2c18f809c861781fff972d637d72e49
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-04-15 00:23:45 +0530

    Validated the user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,13 @@ const {
   loginUser,
   currentUser,
 } = require("../controllers/user.Controller");
+const validateToken = require("../middleware/validateTokenHandler");
 const routes = express.Router();
 
 routes.post("/register", registerUser);
 
 routes.post("/login", loginUser);
 
-routes.get("/current", currentUser);
+routes.get("/current",validateToken, currentUser);
 
 module.exports = routes;
