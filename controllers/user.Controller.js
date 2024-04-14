d195abe6bd53eeb8197c9dcded5bcaacf05a2cd1
Aakash S <aakash@Aakashs-MacBook-Air.local>
2024-04-14 23:52:12 +0530

    Created the registerUser Route

--- controllers/user.Controller.js
+++ controllers/user.Controller.js
@@ -1,7 +1,46 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Register the user" });
+  // Destructuring the username, email, and password from request body
+  const { username, email, password } = req.body;
+
+  // Checking if username, email, or password is missing
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory");
+  }
+
+  // Checking if user with the given email already exists
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User Already registered");
+  }
+
+  // Hashing the password using bcrypt library
+  const hashedPassword = await bcrypt.hash(password, 10);
+  console.log("Hashed Password : ", hashedPassword);
+
+  // Creating a new user with hashed password
+  const newUser = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  console.log(`User created ${newUser}`);
+
+  // If user creation is successful, then we are sending a success response
+  if (newUser) {
+    res.status(201).json({ _id: newUser.id, email: newUser.email });
+  } else {
+    // If user creation fails,then we are sending an error response
+    res.status(400);
+    throw new Error("User data is not Valid");
+  }
 });
 
 const loginUser = asyncHandler(async (req, res) => {
